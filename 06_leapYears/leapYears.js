6c6427b308aa1d7e1dc275762c69f28e2f78cf8e
Josh Cook <jcook2303@gmail.com>
2023-02-18 13:26:16 -0800

    Finished exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(givenYear) {
+    if (givenYear % 4 === 0) {
+        if(givenYear % 100 != 0) {
+            return true;
+        } else if (givenYear % 100 === 0 && givenYear % 400 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
