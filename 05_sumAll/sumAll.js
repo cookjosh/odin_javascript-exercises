a6d267318c4828328fa014e5203a0a7498893e93
Josh Cook <jcook2303@gmail.com>
2023-02-18 12:57:14 -0800

    Finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, lastNumber) {
+    let sum = 0
+    function range(firstNumber, lastNumber) {
+        for (i = firstNumber; i <= lastNumber; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    if (firstNumber < 0 || lastNumber < 0 || typeof firstNumber != "number" || typeof lastNumber != "number") {
+        return "ERROR";
+    } else if (lastNumber < firstNumber) {
+        return range(lastNumber, firstNumber);
+    } else {
+        return range(firstNumber, lastNumber);
+    }
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+/*
+if (typeof firstNumber && typeof lastNumber === "number") {
+        console.log(firstNumber);
+        console.log(lastNumber);
+        console.log(sum);
+        range(firstNumber, lastNumber);
+    }
+*/
