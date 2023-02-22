ea2c3fa9cb6b4bd003682a329cb8b68acc107cd2
Josh Cook <jcook2303@gmail.com>
2023-02-21 22:09:14 -0800

    More exercises.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(input) {
+    bookArray = [];
+    for (i=0; i < input.length; i++) {
+        bookArray.push(input[i].title);
+    }
+    return bookArray;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(input) {
+    let oldest = input.sort(function(a,b) {
+        const firstPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return firstPerson > nextPerson ? -1 : 1;
+      })
+    let oldestPerson = oldest[0];
+    return oldestPerson;
 };
 
 // Do not edit below this line
